e3edc43a170865b85d0e4bef1eb4f5cbe8054740
Alexander Odell <alextrastero@gmail.com>
2018-02-13 11:34:34 +0100

    Fix mocking for localStorage

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import { shallow } from 'enzyme'
 
-import App from './index'
-
 describe('<App />', function () {
-  it('should render without throwing an error', function () {
-    shallow(<App />)
-    expect(1).toEqual(1)
-    // expect(shallow(<App />).contains(<div className='foo'>Bar</div>)).toBe(true)
-    // expect(shallow(<Foo />).is('.foo')).toBe(true)
-    // expect(mount(<Foo />).find('.foo').length).toBe(1)
-    // expect(render(<Foo />).text()).toEqual('Bar')
+  let App;
+  let localStorage
+  let getItemMock
+  let setItemMock
+
+  describe('with one timezone', () => {
+    beforeEach(() => {
+      jest.resetModules()
+
+      setItemMock = jest.fn()
+      getItemMock = jest.fn(() => JSON.stringify(
+        [
+          { title: 'Berlin', timezone: 'Europe/Berlin' }
+        ]
+      ))
+
+      jest.doMock('localStorage', () => ({
+        getItem: getItemMock,
+        setItem: setItemMock
+      }))
+
+      localStorage = require('localStorage')
+      App = require('./index').default
+    })
+
+    it('should render without throwing an error', function () {
+      shallow(<App />)
+      expect(localStorage.getItem).toHaveBeenCalledTimes(1)
+      // expect(shallow(<App />).contains(<div className='foo'>Bar</div>)).toBe(true)
+      // expect(shallow(<Foo />).is('.foo')).toBe(true)
+      // expect(mount(<Foo />).find('.foo').length).toBe(1)
+      // expect(render(<Foo />).text()).toEqual('Bar')
+    })
   })
 })
