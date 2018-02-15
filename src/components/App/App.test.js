c361ad7f615d08798a399828b3d5779b7af0974c
Alexander Odell <alextrastero@gmail.com>
2018-02-15 17:23:19 +0100

    Clean up comments

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -22,10 +22,6 @@ describe('<App />', function () {
 
   it('should render without throwing an error', () => {
     expect(wrapper).toBeDefined()
-    // expect(shallow(<App />).contains(<div className='foo'>Bar</div>)).toBe(true)
-    // expect(shallow(<Foo />).is('.foo')).toBe(true)
-    // expect(mount(<Foo />).find('.foo').length).toBe(1)
-    // expect(render(<Foo />).text()).toEqual('Bar')
   })
 
   it('should fetch timezones from localStorage', () => {
